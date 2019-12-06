1ac8c0ab4787d07056655bf789738886206782b9
Carlos Arranz XPS15 <carlos_arranz@yahoo.com>
2019-12-06 23:16:18 +0100

    Controller, modificado el método que gestiona las peticiones en send para usar el nuevo WatsonAssistantService

--- backend/src/main/java/watsonassistant/WatsonAssistantController.java
+++ backend/src/main/java/watsonassistant/WatsonAssistantController.java
@@ -1,14 +1,28 @@
 package watsonassistant;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class WatsonAssistantController {
 
-    @RequestMapping(value="/send", method = RequestMethod.GET)
-    public String send(@RequestParam("message") String message) {
+    private WatsonAssistantService watsonAssistantService;
 
-        return message;
+    @Autowired
+    public WatsonAssistantController(WatsonAssistantService watsonAssistantService) {
+        this.watsonAssistantService = watsonAssistantService;
+    }
+
+    @RequestMapping(value="/send", method = RequestMethod.GET)
+    public JsonNode send(@RequestParam("message") String message) throws JsonProcessingException {
+
+        //String response1 = watsonAssistantService.sendMessage("Hola");
+        WatsonAssistantMessage response;
+        response = watsonAssistantService.sendMessage(message);
+
+        return response.getGeneric();
 
     }
 
